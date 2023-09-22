4c0c829ccf06675f71b4cd13760e7be0b14fd001
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-09-22 21:57:13 +0400

    refactor base.py: add methods to Base

--- base.py
+++ base.py
@@ -1,7 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from typing import Union
 import bson
 
 
 class Base(BaseModel):
     _id: Union[bson.objectid.ObjectId, int] = -1
+
+    def serialize(self):
+        pass
+
+    @classmethod
+    def unserialize(cls, serialized_data: dict) -> Base:
+        try:
+            return cls(**serialized_data)
+        except ValidationError as e:
+            print(e)
