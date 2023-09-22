1f94d3bcf90b6d2d91d98c3a2d77807cb0fb7f01
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-09-22 21:43:19 +0400

    refactor db.py: add methods to class MongoDb

--- db.py
+++ db.py
@@ -13,3 +13,26 @@ class Db:
 class MongoDb(Db):
     db = MongoClient(var_name)
 
+    def insert_one(self):
+        pass
+
+    def insert_many(self):
+        pass
+
+    def find_one(self):
+        pass
+
+    def find_many(self):
+        pass
+
+    def update_one(self):
+        pass
+
+    def update_many(self):
+        pass
+
+    def delete_one(self):
+        pass
+
+    def delete_many(self):
+        pass
