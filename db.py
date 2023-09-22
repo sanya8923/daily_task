86c1ce9ffe91b63e59e4b5fe9a0dcfd1691ac5fe
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-09-22 22:42:14 +0400

    add documentation to db.py

--- db.py
+++ db.py
@@ -11,20 +11,44 @@ cluster = MongoClient(var_name)
 
 
 class Db:
+    """
+    Base class for database interaction.
+    """
     pass
 
 
 class MongoDb(Db):
+    """
+    MongoDB's interaction class, inheriting from the base Db class.
+    Provides methods for basic CRUD operations.
+    """
     db = cluster['daily_task']
     collection = db['tasks']
 
     def insert_one(self, data: dict, *args, **kwargs) -> InsertOneResult:
+        """
+        Inserts a single document into the collection.
+
+        :param data: The document to be inserted.
+        :return: The result of the insertion.
+        """
         return self.collection.insert_one(data)
 
     def find_one(self, fltr: Dict[str, Any], *args, **kwargs) -> Optional['Model']:
+        """
+        Finds a single document in the collection.
+
+        :param fltr: The filter to apply for the search.
+        :return: The document found or None if no document matches the filter.
+        """
         return self.collection.find_one(fltr)
 
     def find_many(self, *args, **kwargs) -> Optional[List['Model']]:
+        """
+        Finds multiple documents in the collection.
+
+        :return: A list of documents found or None if no documents match the filter.
+        """
         result = []
         cursor = self.collection.find(*args, **kwargs)
 
@@ -38,8 +62,21 @@ class MongoDb(Db):
         else:
             return None
 
-    def update_one(self, fltr: Dict[str, Any], update: Dict[str, Any]) -> Optional[UpdateResult]:
+    def update_one(self, fltr: Dict[str, Any], update: Dict[str, Any], *args, **kwargs) -> Optional[UpdateResult]:
+        """
+        Updates a single document in the collection.
+
+        :param fltr: The filter to apply for the search.
+        :param update: The modifications to apply.
+        :return: The result of the update.
+        """
         return self.collection.update_one(fltr, update)
 
-    def delete_one(self, fltr: Dict[str, Any]) -> Optional[DeleteResult]:
+    def delete_one(self, fltr: Dict[str, Any], *args, **kwargs) -> Optional[DeleteResult]:
+        """
+        Deletes a single document from the collection.
+
+        :param fltr: The filter to apply for the search.
+        :return: The result of the deletion.
+        """
         return self.collection.delete_one(fltr)
