d118814f91b3aaca5593539e1ea76352f7aa7351
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-09-24 22:13:44 +0400

    add class DbManager and methods

---
+++ db_manager.py
@@ -0,0 +1,24 @@
+from db import Db
+from pymongo.results import InsertOneResult, UpdateResult, DeleteResult
+from typing import Optional, List, Dict, Any
+
+
+class DbManager:
+    def __init__(self, db: Db):
+        self.db = db
+
+    def load_one(self, data: dict) -> Optional[InsertOneResult]:
+        return self.db.insert_one(data)
+
+    def get_one(self, fltr: dict) -> Optional['Model']:
+        return self.db.find_one(fltr)
+
+    def get_many(self, *args, **kwargs) -> Optional[List['Model']]:
+        return self.db.find_many(*args, **kwargs)
+
+    def update_one(self, fltr: Dict[str, Any], update: Dict[str, Any], *args, **kwargs) -> Optional[UpdateResult]:
+        return self.db.update_one(fltr, update, *args, **kwargs)
+
+    def delete_one(self, fltr: Dict[str, Any], *args, **kwargs) -> Optional[DeleteResult]:
+        return self.db.delete_one(fltr, *args, **kwargs)
+
