477069def779ed1f286d03ab04dbd50b8710975c
FIRST_NAME LAST_NAME <sanya8923@gmail.com>
2023-09-25 01:26:28 +0400

    add documentation to display.py

--- display.py
+++ display.py
@@ -16,20 +16,38 @@ BACKGROUND = '#242424'
 
 
 class Display(ABC):
+    """
+    Abstract base class representing a display.
+    """
     def __init__(self):
+        """
+        Initialize a new Display instance.
+        """
         self.app = Tk()
 
     @abstractmethod
     def make(self):
+        """
+        Abstract method to make the display.
+        """
         pass
 
 
 class MainDisplay(Display):
+    """
+    Main display class representing the application window.
+    """
     def __init__(self):
+        """
+        Initialize a new MainDisplay instance.
+        """
         super().__init__()
         self.name_app = NAME_APP
 
     def make(self):
+        """
+        Create and show the main display.
+        """
         try:
             self.app.title(NAME_APP)
             frame = Frame(self.app, background=BACKGROUND)
@@ -55,14 +73,21 @@ class MainDisplay(Display):
         except Exception as e:
             with open('log.txt', 'a') as log_file:
                 log_file.write(f"Exception: {e}\n")
-            messagebox.showerror('Error', 'A graphical error occurred. Please try again or contact support.')
+            messagebox.showerror('Error', 'An unexpected error occurred. Please try again or contact support.')
             self.app.quit()
 
     def add_element(self, display_element: DisplayElement, frame: Frame):
+        """
+        Add a display element to the frame.
+
+        :param display_element: The display element to be added.
+        :param frame: The frame to which the display element will be added.
+        :return: The result of adding the display element.
+        """
         try:
             return display_element.add(frame)
         except Exception as e:
             with open('log.txt', 'a') as log_file:
                 log_file.write(f"Exception: {e}\n")
-            messagebox.showerror('Error', 'A graphical error occurred. Please try again or contact support.')
+            messagebox.showerror('Error', 'An unexpected error occurred. Please try again or contact support.')
             self.app.quit()
